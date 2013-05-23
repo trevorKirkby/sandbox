c12cc77952072d60436db80d65f0b2ec3ee9584e
Trevor Kirkby <TrevorKirkby123@gmail.com>
2013-05-23 10:03:08 +0000

    enter error fixed some directories added

--- sandbox/session.py
+++ sandbox/session.py
@@ -12,6 +12,8 @@ class Session:
                 cmdline = raw_input(prompt)
                 # Break the command line into an argv list
                 argv = cmdline.split()
+                if len(argv) == 0:
+                    continue
                 # Look for a handler that matches argv[0]
                 handler = getattr(self,argv[0]+'_handler',None)
                 if handler:
@@ -24,11 +26,25 @@ class Session:
     def exit_handler(self,argv):
         print 'exit'
         raise SystemExit
+
     def pwd_handler(self,argv):
         print '/'.join(self.pwd.abspath())
+
     def ls_handler(self,argv):
         for child in self.pwd.children:
             print ('%-16s' % child.name),
         print
+
     def whoami_handler(self,argv):
         print 'seriously??'
+
+'''
+    def goto_handler(self,argv):
+        realDirectory = False
+        for item in self.pwd.children:
+            if item == argv[1]:
+                self.pwd = item
+                realDirectory = True
+        if realDirectory == False:
+            print name, "goto:", argv[1], "no such file or directory",
+'''
--- sbox.py
+++ sbox.py
@@ -1,22 +1,33 @@
 import sandbox.filesystem as fs
 import sandbox.session
 
-sbox = fs.Filesystem()
+
+rootdir = fs.Filesystem()
 
 users = fs.Directory('users')
-sbox.add(users)
+rootdir.add(users)
 
 trevor = fs.Directory('trevor')
 users.add(trevor)
+
+desktop = fs.Directory('desktop')
+trevor.add(desktop)
+
+filething = fs.File('file.txt','blub blub im a fish')
+desktop.add(filething)
+
 a = fs.File('a.txt','The quick brown fox...')
 trevor.add(a)
+
 b = fs.File('b.txt','...jumped over the lazy dog')
 trevor.add(b)
 
 ryan = fs.Directory('ryan')
 users.add(ryan)
+
 c = fs.File('c.txt','Here I am!')
 ryan.add(c)
 
-s = sandbox.session.Session(sbox,'mash',trevor)
+
+s = sandbox.session.Session(rootdir,'mash',trevor)
 s.shell('> ')
